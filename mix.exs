5b9a1628e572653e841b4b25cbd97da28bdb8e65
tayberrs <19411968+tayberrs@users.noreply.github.com>
2023-06-28 21:09:16 -0600

    deps for graphql

--- mix.exs
+++ mix.exs
@@ -48,7 +48,9 @@ defmodule Fenix.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.7.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
